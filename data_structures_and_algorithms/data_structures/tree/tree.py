69ee56aadb444e6785c7880c60a202e655244f35
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-10-04 00:41:02 +0300

    add methods

--- data_structures_and_algorithms/data_structures/tree/tree.py
+++ data_structures_and_algorithms/data_structures/tree/tree.py
@@ -26,6 +26,19 @@ class BinaryTree:
         except ValueError as e:
             print(f"error : {e}")
 
+    def pre_order(self, root=None, arr=None):
+        """ Root left right """
+        try:
+            if arr == None:
+                arr = []
+            arr.append(root.value)
+            if root.left:
+                self.pre_order(root.left, arr)
+            if root.right:
+                self.pre_order(root.right, arr)
+            return arr
+        except AttributeError:
+            return "A root element parameter is required"
 
 
     def inOrder(self):
@@ -87,7 +100,28 @@ class BinarySearchTree(BinaryTree):
                             break
                         current = current.right
         except ValueError as e:
-            print(f"error : {e}")        
+            print(f"error : {e}")  
+
+
+    def new_add(self,value):
+        if self.root == None:
+            self.root = Node(value)
+        
+        curr = self.root
+        while curr:
+            if value < curr.value:
+                if curr.left == None:
+                    curr.left = Node(value)
+                    break
+                curr = curr.left
+
+            else:
+                if curr.right == None:
+                    curr.right = Node(value)
+                    break
+                curr = curr.right
+    
+
     
     
     def contains(self, value):
@@ -113,16 +147,63 @@ class BinarySearchTree(BinaryTree):
         except ValueError as e:
             print(f"error : {e}") 
 
+    def cont(self,value):
+        curr = self.root
+        if curr.value == value:
+            return True
+        while curr:
+            if value == curr.value:
+                return True
+            elif value < curr.value:
+                curr = curr.left
+            elif value >curr.value:
+                curr = curr.right
+        return False
+
+    def max_val_bs(self):
+        
+        if self.root == None:
+            return None
+        curr = self.root
+        while curr.right:
+            curr = curr.right
+        return curr.value
+
+    def max_val(self):
+        try:    
+            """
+            Pre-order: root >> left >> right
+            """
+            maxx = [self.root.value]
+            def _walk(node):
+                
+                if not node:
+                    return
+                if node.value > maxx[0]:
+                    maxx[0] = node.value
+                _walk(node.left)
+                _walk(node.right)
+            _walk(self.root)
+            return maxx[0]
+        except ValueError as e:
+            print(f"error : {e}")        
+            
+
+ 
 
 
 if __name__=='__main__':
-    mohd = BinarySearchTree()
-    mohd.add(1)
-    mohd.add(2)
-    mohd.add(3)
-    mohd.add(4)
-    mohd.add(5)
+    tree = BinarySearchTree()
+    tree.root = Node(2)
+    tree.root.left = Node(70)
+    tree.root.right = Node(5)
+    tree.root.left.left = Node(2)
+    tree.root.left.right = Node(20)
+    tree.root.right.right = Node(9)
+    tree.root.right.right.left = Node(9)
+    tree.root.left.right.left = Node(5)
+    tree.root.left.right.right = Node(11)
     
-    print(mohd.postOrder())
-    print(mohd.inOrder())
-    print(mohd.preOrder())
+    # print(tree.postOrder())
+    # print(tree.inOrder())
+    print(tree.max_val())
